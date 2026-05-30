9e706e3a0947c83b85f385fbd8f5f3b4ce3ecd23
andreaccamachoj <andreaccamachoj@gmail.com>
2026-05-30 07:57:08 -0500

    first commit

---
+++ src/main/java/com/mgads/appmoviles/classtrack/ClasstrackApplication.java
@@ -0,0 +1,13 @@
+package com.mgads.appmoviles.classtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClasstrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClasstrackApplication.class, args);
+	}
+
+}
